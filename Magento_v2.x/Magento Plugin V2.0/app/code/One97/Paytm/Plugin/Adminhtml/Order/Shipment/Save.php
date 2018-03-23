7d563632963f945e937b950a8f45a22cc4aff2c0
Pranav <pranav@shoptimize.in>
2018-03-23 16:04:09 +0530

    added plugin to force set Order status to "Complete" after all shipments for given order are generated

---
+++ Magento_v2.x/Magento Plugin V2.0/app/code/One97/Paytm/Plugin/Adminhtml/Order/Shipment/Save.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace One97\Paytm\Plugin\Adminhtml\Order\Shipment;
+
+use Psr\Log\LoggerInterface;
+use Magento\Sales\Model\OrderFactory;
+
+class Save
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $_logger;
+
+    /**
+     * @var OrderFactory
+     */
+    protected $_order;
+
+    /**
+     * UpdateAttributes constructor.
+     */
+    public function __construct(
+        OrderFactory $order,
+        LoggerInterface $logger
+    )
+    {
+        $this->_order  = $order;
+        $this->_logger = $logger;
+    }
+
+    public function afterExecute(
+        \Magento\Shipping\Controller\Adminhtml\Order\Shipment\Save $save
+    )
+    {
+        $order = null;
+        try {
+            $order_id = $save->getRequest()->getParam('order_id');
+            $order = $this->_order->create()->load($order_id);
+        } catch (\Exception $e) {
+        }
+        if(!$order) {
+            $this->_logger->debug("Order not found");
+        } else {
+            if (!$order->canUnhold() || !$order->canShip() || !$order->isCanceled() || !$order->canInvoice()) {
+                if($order->getStatus() != $order::STATE_COMPLETE) {
+                    $order->setStatus($order::STATE_COMPLETE);
+                    $order->setState($order::STATE_COMPLETE);
+                    $order->save();
+                }
+
+            }
+        }
+    }
+}
